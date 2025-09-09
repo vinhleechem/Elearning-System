b4d9342c39893d36419d0273e7387297eda7f2e4
Vinh Le <vinhpq.dt.468@gmail.com>
2025-09-09 21:13:23 +0700

    [UPDATE] code BE

--- BackEnd/src/main/java/org/example/elearning/ELearningApplication.java
+++ BackEnd/src/main/java/org/example/elearning/ELearningApplication.java
@@ -1,13 +1,13 @@
-package org.example.backend;
+package org.example.elearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackEndApplication {
+public class ELearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackEndApplication.class, args);
+		SpringApplication.run(ELearningApplication.class, args);
 	}
 
 }
--- BackEnd/src/test/java/org/example/elearning/ELearningApplicationTests.java
+++ BackEnd/src/test/java/org/example/elearning/ELearningApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.backend;
+package org.example.elearning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackEndApplicationTests {
+class ELearningApplicationTests {
 
 	@Test
 	void contextLoads() {
